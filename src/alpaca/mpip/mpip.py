c88f023df9da3a62e60147c7d613c8c1c29a05ca
kuen <tobias.kuen@fau.de>
2025-06-25 14:46:08 +0200

    set settings infinity for scip implied calculation

--- src/alpaca/mpip/mpip.py
+++ src/alpaca/mpip/mpip.py
@@ -23,7 +23,7 @@ class MPIP:  # pylint: disable=too-many-instance-attributes
         self.relation = {}
         self.implying_function = scip.Expr()
         self.interval_lp = scip.Model()
-        self.interval_lp.hideOutput()
+        # self.interval_lp.hideOutput()
         self.interval_lp_implying_vars = {}
         self.interval_lp_implied_var = 0
         self.separator = None
@@ -39,7 +39,7 @@ class MPIP:  # pylint: disable=too-many-instance-attributes
         self.implied_id = implied_id
         self.implied_breakpoints = breakpoints
         self.interval_lp_implied_var = self.interval_lp.addVar(
-            f"x_{self.implied_id}", lb=None
+            f"x_{self.implied_id}", lb=-s.StaticSettings.infinity
         )
 
     def add_implying_id(self, implying_id: str, breakpoints: list[float]) -> None:
@@ -83,6 +83,7 @@ class MPIP:  # pylint: disable=too-many-instance-attributes
             self.interval_lp.chgVarUb(var, high)
 
         self.interval_lp.setObjective(self.interval_lp_implied_var, "minimize")
+        self.interval_lp.writeProblem("model.lp")
         self.interval_lp.optimize()
         lower_bound = round(
             self.interval_lp.getObjVal(), s.StaticSettings.rounding_precision
@@ -90,6 +91,7 @@ class MPIP:  # pylint: disable=too-many-instance-attributes
         self.interval_lp.freeTransform()
 
         self.interval_lp.setObjective(self.interval_lp_implied_var, "maximize")
+        self.interval_lp.writeProblem("model.lp")
         self.interval_lp.optimize()
         upper_bound = round(
             self.interval_lp.getObjVal(), s.StaticSettings.rounding_precision
