e0f18982c0a90e2fd33cb1b3a8e4fe8c8c4cd4b6
kuen <tobias.kuen@fau.de>
2025-07-15 13:11:48 +0200

    add piecewise constant approximation to bilinear and multilinear terms

--- src/alpaca/expressions/multilinear_expression.py
+++ src/alpaca/expressions/multilinear_expression.py
@@ -2,7 +2,10 @@
 """
 @authors: kuen,
 """
-from alpaca.model_data import variable as var
+import itertools
+import bisect
+
+from alpaca.model_data import variable as var, constraint as con
 import alpaca.expressions.expression as exn
 
 
@@ -41,12 +44,64 @@ class MultilinearExpression(exn.Expression):
         """
         super().__init__(name, model_data, level, representative_variable)
         self.variables = variables
+        self.model_data = model_data
         self.representative_variable.add_nonlinearity_to_occurring_in("multilinear")
+        self.piecewise_constant_relation = {}
         for variable in self.variables:
             variable.add_nonlinearity_to_occurring_in("multilinear")
 
     def apply_piecewise_constant_approximation(self):
-        """Apply piecewise constant approximation."""
+        """Apply piecewise constant approximation for multilinear expressions."""
+        all_variables_mid_values_with_indices = []
+        for variable in self.variables:
+            mid_values_for_current_var = []
+            for i, breakpoint_val in enumerate(variable.breakpoints[:-1]):
+                mid_value = (variable.breakpoints[i + 1] + breakpoint_val) / 2
+                mid_values_for_current_var.append((mid_value, i))
+            all_variables_mid_values_with_indices.append(mid_values_for_current_var)
+
+        for combination_with_indices in itertools.product(
+            *all_variables_mid_values_with_indices
+        ):
+            implied_value = 1.0
+            current_variable_indices = []
+
+            for mid_value, index in combination_with_indices:
+                implied_value *= mid_value
+                current_variable_indices.append(index)
+            implied_index = bisect.bisect_left(
+                self.representative_variable.breakpoints, implied_value
+            )
+            self.piecewise_constant_relation[tuple(current_variable_indices)] = (
+                implied_index,
+            )
+            constraint_variables = [
+                (
+                    -1.0,
+                    self.representative_variable.pwl_variables_binary[implied_index],
+                )
+            ]
+
+            for var_idx, index_in_combination in enumerate(current_variable_indices):
+                constraint_variables.append(
+                    (
+                        1.0,
+                        self.variables[var_idx].pwl_variables_binary[
+                            index_in_combination
+                        ],
+                    )
+                )
+            self.model_data.constraints.update(
+                {
+                    f"mc_{self.name}_"
+                    f"{'_'.join(map(str, current_variable_indices))}": con.Constraint(
+                        f"mc_{self.name}_{'_'.join(map(str, current_variable_indices))}",
+                        con_type="==",
+                        variables=constraint_variables,
+                        rhs=1.0,
+                    )
+                }
+            )
 
     def propagate_variable_bounds(self):
         """Propagate variables bounds."""
